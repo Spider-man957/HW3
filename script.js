8d3a51194eca29ff502f3a088375490d3322a6c3
Spider-man957 <itprogram88@gmail.com>
2025-09-26 18:15:19 +0300

    HW3

---
+++ script.js
@@ -0,0 +1,18 @@
+let result;
+result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+let email;
+email = "itprogram88@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+let a = "My ";
+let b = "Name ";
+let c = "is ";
+let fullName = console.log(a + b + c + "Viktor");
+
+let userName = prompt("Input your name... Please)");
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
